97cf286c9a2ec49a7f9b031636ae7a4b504521c3
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-10 12:19:08 -0500

    Initial Commit

---
+++ ProduceView/ProduceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProduceViewApp.swift
+//  ProduceView
+//
+//  Created by Evelyn Chen on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct ProduceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
